be562386feb19f035fa32ad257c778562e62d8ae
Daniel Rodríguez Chavarría <danielrodriguezchavarria@ccia-14-194.etsii>
2017-07-20 20:03:49 +0200

    ReadingF documentation

--- src/ReadingF.hs
+++ src/ReadingF.hs
@@ -12,98 +12,82 @@ import qualified Data.Set as S
 import PolAux (PolF2, expTo1, var, zerov)
 import Tool (tool)
 
+-------------------------------------------------------------------------------
+-- | __(clause2pol cs)__ is a pair /(p,vs)/, where /p/ is the polynomial that
+-- corresponds to the clause /cs/ (which is written in DIMACS format) and /vs/ is
+-- the set of its variables.
+--
+-- >>> clause2pol ["1"]
+-- (x1,fromList [x1])
+-- >>> clause2pol ["1","-2"]
+-- (x1x2+x2+1,fromList [x1,x2])
 
--- main1 :: FilePath -> IO ()
--- main1 f = do
---   s <- readFile f
---   print
---     $ nub
---     $ foldr (\x acc -> (((cleanExp . varFold . words) x):acc)) []
---     $ lines
---     $ s
+clause2pol :: [String] -> (PolF2, S.Set (PolF2))
+clause2pol (c:cs) | c == "c" || c == "p" = (1, S.empty)
+clause2pol cs = aux $ foldl' (\acc x -> disj (var' x) acc) (zerov,S.empty) cs
+  where aux (a,b) = (expTo1 a,b)
+        disj (x,v) (y,vs) | v == zerov = (y,vs)
+                          | otherwise   = (x + y + x*y, S.insert v vs)
 
--- -- main "/Users/danielrodriguezchavarria/Desktop/300/ReadingFiles/fil3.txt"
+-------------------------------------------------------------------------------
 
--- main2 :: FilePath -> IO ()
--- main2 f = do
---   s <- readFile f
---   print
---     $ tool
---     $ nub
---     $ foldr (\x acc -> (((cleanExp . varFold . words) x):acc)) []
---     $ lines
---     $ s 
+-- | __(var' lit)__ is the pair /(p,v)/, where /p/ is the polynomial and /v/ is
+-- the variable that corresponds (if it does) to the literal /lit/ (in DIMACS
+-- format).
+--
+-- >>> var' "0"
+-- (0,0)
+-- >>> var' "1"
+-- (x1,x1)
+-- >>> var' "-1"
+-- (x1+1,x1)
 
-cleanP (a,b) = (expTo1 a,b)
+var' :: String -> (PolF2,PolF2)
+var' "0"       = (zerov,zerov)
+var' ('-':lit) = (1 + x,x)
+                   where x = var ('x':lit)
+var' lit       = (x,x)
+                   where x = var ('x':lit)
 
-insertP (a,b) (acc,vs) = (S.insert a acc, S.union vs b)
+-------------------------------------------------------------------------------
 
-insertP' (a,b) (acc,vs) = (S.insert (a,b) acc, S.union vs b)
+-- | __(dimacs2pols f)__ is the pair (/ps/,/vs/) where ps is the set of polynomials
+-- wich corresponds to the formula in DIMACS format writed in the file /f/ and
+-- /vs/ is the set of variables wich occurs in any polynomial.
+--
+-- >>> dimacs2pols "exDIMACS/easy/example1.txt"
+-- (fromList [x1x2+x1+x2,1],fromList [x1,x2])
+-- >>> dimacs2pols "exDIMACS/easy/example4.txt"
+-- (fromList [x1x2+x1+x2,x1x2+x1+1,x1x2+x2+1,x1x2+1,1],fromList [x1,x2])
 
-main3 f = do
+dimacs2pols f = do
   s <- readFile f
-  print $ (foldr (\x acc -> (S.insert ((expTo1 . varFold . words) x) acc)) S.empty) $ lines $ s
+  print $ 
+    (foldr (\x acc -> (insertPol ((clause2pol . words) x) acc))
+           (S.empty,S.empty)) $
+      lines $
+        s
+     where insertPol (a,b) (acc,vs) = (S.insert a acc, S.union vs b)
+           
+-------------------------------------------------------------------------------
 
-main3P f = do
+-- | __(main f)__ is verified if the set of formulas in DIMACS format in the
+-- file /f/ were satisfiable. Otherwise, /(main f)/ would return False.
+--
+-- >>> main "exDIMACS/easy/example1.txt"
+-- True
+-- >>> main "exDIMACS/easy/example4.txt"
+-- False
+
+main f = do
   s <- readFile f
-  print $ snd $ (foldr (\x acc -> (insertP ((cleanP . varFoldP . words) x) acc)) (S.empty,S.empty)) $ lines $ s
--------------------------------------------------------------------------------
-
--- main2 "/Users/danielrodriguezchavarria/Desktop/300/ReadingFiles/fil3.txt"
-
--- main4 f = do
---   s <- readFile f
---   print $ tool1 $ (foldr (\x acc -> (S.insert ((expTo1 . varFold . words) x) acc)) S.empty) $ lines $ s
-
-main4P f = do
-  s <- readFile f
-  print $ tool $ (foldr (\x acc -> (insertP ((cleanP . varFoldP . words) x) acc)) (S.empty,S.empty)) $ lines $ s
-
--- main4SP f = do
---   s <- readFile f
---   print $ toolSP $ (foldr (\x acc -> (insertP' ((cleanP . varFoldP . words) x) acc)) (S.empty,S.empty)) $ lines $ s
-
--------------------------------------------------------------------------------
-  
--- Si es FND usar:
-
--- divide :: [String] -> [[String]]
--- divide [] = [[]]
--- divide ((x:xs):xss) | x == '-'  = map (xs:) yss ++ yss
---                     | otherwise = map ((x:xs):) yss
---   where yss = divide xss
+  print $
+    tool $
+      (foldr (\x acc -> (insertPol ((clause2pol . words) x) acc))
+             (S.empty,S.empty)) $
+        lines $
+          s
+   where insertPol (a,b) (acc,vs) = (S.insert  a acc, S.union vs b)
 
 -------------------------------------------------------------------------------
 
--- Si es FNC usar:
-
-varFold :: [String] -> PolF2
-varFold (x:xs) | x == "c" || x == "p" = 1
-varFold xs = foldl' (\acc x -> disj (var' x) acc) zerov xs
-
-varFoldP :: [String] -> (PolF2, S.Set (PolF2))
-varFoldP (x:xs) | x == "c" || x == "p" = (1, S.empty)
-varFoldP xs = foldl' (\acc x -> disjP (var'P x) acc) (zerov,S.empty) xs
-
--------------------------------------------------------------------------------
-
-var' "0"      = zerov
-var' ('-':xs) = 1 + var xs
-var' x        = var x
-
-
-var'P "0"      = (zerov,zerov)
-var'P ('-':xs) = (1 + x,x)
-                   where x = var xs
-var'P xs       = (x,x)
-                   where x = var xs    
-
--------------------------------------------------------------------------------
-
-disj :: Num a => a -> a -> a
-disj x y = x + y + x*y
-
-
-disjP (x,v) (y,vs) | v == zerov = (y,vs)
-                   | otherwise   = (x + y + x*y, S.insert v vs)
--------------------------------------------------------------------------------
--- src/Tool.hs
+++ src/Tool.hs
@@ -7,7 +7,7 @@ module Tool
     ( tool
     ) where
 
-import Data.List(nub,iterate,partition, foldl', union)
+import Data.List (nub,iterate,partition, foldl', union)
 
 import PolAux
 import Examples
@@ -32,14 +32,14 @@ import qualified Data.Set as S
 
 deltaRule :: PolF2 -> PolF2 -> PolF2 -> PolF2
 deltaRule p a1 a2 = expTo1 (aux + a1a2 + aux2)
-  where da1        = deriv a1 p
-        da2        = deriv a2 p
-        a1a2       = expTo1 (a1*a2)
-        a1da2      = expTo1 a1*da2
-        a2da1      = expTo1 a2*da1
-        da1da2     = expTo1 da1*da2
-        aux        = expTo1 (a1da2 + a2da1 + da1da2)
-        aux2       = expTo1 (a1a2*aux)
+  where da1       = deriv a1 p
+        da2       = deriv a2 p
+        a1a2      = expTo1 (a1*a2)
+        a1da2     = expTo1 a1*da2
+        a2da1     = expTo1 a2*da1
+        da1da2    = expTo1 da1*da2
+        aux       = expTo1 (a1da2 + a2da1 + da1da2)
+        aux2      = expTo1 (a1a2*aux)
 
 -------------------------------------------------------------------------------
 
@@ -64,18 +64,23 @@ deltaRule1Step v pps acum | S.null pps = acum
 
 -------------------------------------------------------------------------------
                                    
--- | __(tool (vvs,ps))__ is verified if the original set of formulas which
+-- | __(tool (ps,vvs))__ is verified if the original set of formulas which
 -- polynomials from /ps/ came was satisfiable. Otherwise, the function will
 -- return False if that set of formulas was unsatisfiable. Note that /vvs/ is
--- the set of variables which occurs in any polynomial from /ps/.
+-- the set of variables which occurs in any polynomial from /ps/. For example,
+--
+-- >>> tool (S.fromList[1],S.empty)
+-- True
+-- >>> tool (S.fromList[x1,x1+1],S.fromList[x1])
+-- False
 
 -- We should think if there exists any way to use the lazy power in the search
 -- of zeros.
 
 tool :: (S.Set (PolF2), S.Set (PolF2)) -> Bool
-tool (vvs,ps) | S.null vvs        = S.notMember 0 ps
+tool (ps,vvs) | S.null vvs     = S.notMember 0 ps
               | S.member 0 ps  = False
-              | otherwise      = tool (vs,ps')
+              | otherwise      = tool (ps',vs)
           where (v,vs)    = S.deleteFindMin vvs
                 --(ps1,ps2) = S.partition (\p -> aux (vars p)) ps -- time leak?
                 (ps1,ps2) = S.partition (\p -> mdivides (lm v) (lm p)) ps
