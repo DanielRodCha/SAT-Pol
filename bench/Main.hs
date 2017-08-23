d8bfaa540644ddefda7153ae8e8a46752ecc7e69
Daniel Rodríguez Chavarría <danielrodriguezchavarria@MacBook-Air-de-Daniel.local>
2017-08-23 17:54:53 +0200

    benchamarks OK

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,13 +1,29 @@
+module Main where
+
 import Criterion.Main
 import qualified ReadingF as RF
 
 main = defaultMain [
-  bgroup "dimacs2pols" [
-      bench "easy1" $ whnfIO $ RF.dimacs2pols "exDIMACS/easy/example1.txt"
-    , bench "easy2" $ whnfIO $ RF.dimacs2pols "exDIMACS/easy/example2.txt"
-    , bench "easy3" $ whnfIO $ RF.dimacs2pols "exDIMACS/easy/example3.txt"
-    , bench "easy4" $ whnfIO $ RF.dimacs2pols "exDIMACS/easy/example4.txt"
-                        ]
+   bgroup "dimacs2pols" [
+       bench "easy1" $ whnf RF.dimacs2pols "exDIMACS/easy/example1.txt"
+     , bench "easy2" $ whnf RF.dimacs2pols "exDIMACS/easy/example2.txt"
+     , bench "easy3" $ whnf RF.dimacs2pols "exDIMACS/easy/example3.txt"
+     , bench "easy4" $ whnf RF.dimacs2pols "exDIMACS/easy/example4.txt"
+                         ]
+  -- , bgroup "var'" [
+  --      bench "0"  $ whnf RF.var' "0"
+  --    , bench "1"  $ whnf RF.var' "1"
+  --    , bench "-1" $ whnf RF.var' "-1"
+  --    , bench "0"  $ whnf RF.var' "10"
+  --    , bench "1"  $ whnf RF.var' "-10"
+  --    , bench "-1" $ whnf RF.var' "100"
+  --                        ]
+   , bgroup "main" [
+       bench "easy1" $ whnf RF.main "exDIMACS/easy/example1.txt"
+     , bench "easy2" $ whnf RF.main "exDIMACS/easy/example2.txt"
+     , bench "easy3" $ whnf RF.main "exDIMACS/easy/example3.txt"
+     , bench "easy4" $ whnf RF.main "exDIMACS/easy/example4.txt"
+                    ]
   ]
 
 
