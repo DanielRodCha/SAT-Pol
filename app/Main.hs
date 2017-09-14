7c110d1764d35a5dbaf49ff2412ee64c2787f366
DanielRodCha <danielrodcha@icloud.com>
2017-09-14 12:28:25 +0200

    add executable sat0, but is sat1

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Tool
 
 main :: IO ()
 main = do
-  s <- readFile "exDIMACS/medium/exampleSat0.txt"
+  s <- readFile "exDIMACS/medium/exampleSat1.txt"
   print $
     toolN $
       (foldr (\x acc -> (S.insert ((clause2pol' . words) x) acc))
